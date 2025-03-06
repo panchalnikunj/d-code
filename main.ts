074c40b784fbf242cf5a178420b553d8de24b5f4
Nikunj Panchal <59598242+panchalnikunj@users.noreply.github.com>
2025-03-06 19:27:25 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,211 @@
-//% color=#0000FF icon="\uf26c" block="I2C LCD"
-namespace I2CLCD {
+/**
+* makecode I2C LCD1602 package for microbit.
+* From microbit/micropython Chinese community.
+* http://www.micropython.org.cn
+*/
 
-    let LCD_I2C_ADDR = 0x27 // Default I2C Address, change if needed
+/**
+ * Custom blocks
+ */
+//% weight=20 color=#0fbc11 icon="▀"
+namespace I2C_LCD1602 {
+    let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
+    let BK: number      // backlight control
+    let RS: number      // command/data
 
-    // Send command to LCD
-    function lcdCommand(cmd: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = 0x00
-        buf[1] = cmd
-        pins.i2cWriteBuffer(LCD_I2C_ADDR, buf)
-        basic.pause(5)
+    // set LCD reg
+    function setreg(d: number) {
+        pins.i2cWriteNumber(i2cAddr, d, NumberFormat.Int8LE)
+        basic.pause(1)
     }
 
-    // Send character to LCD
-    function lcdWriteChar(ch: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = 0x40
-        buf[1] = ch
-        pins.i2cWriteBuffer(LCD_I2C_ADDR, buf)
-        basic.pause(5)
+    // send data to I2C bus
+    function set(d: number) {
+        d = d & 0xF0
+        d = d + BK + RS
+        setreg(d)
+        setreg(d + 4)
+        setreg(d)
     }
 
-    // Initialize LCD
-    //% block="Initialize LCD at address %addr"
-    //% group="LCD Display"
-    export function lcdInit(addr: number): void {
-        LCD_I2C_ADDR = addr
-        basic.pause(50)
-        lcdCommand(0x33) // Initialize
-        lcdCommand(0x32)
-        lcdCommand(0x28) // Function set: 4-bit mode, 2 lines, 5x8 dots
-        lcdCommand(0x0C) // Display ON, cursor OFF
-        lcdCommand(0x06) // Entry mode set: Move cursor right
-        lcdCommand(0x01) // Clear display
-        basic.pause(5)
+    // send command
+    function cmd(d: number) {
+        RS = 0
+        set(d)
+        set(d << 4)
     }
 
-    // Write text to LCD at a given position
-    //% block="Write %text on LCD at column %col row %row"
-    //% col.min=0 col.max=15 row.min=0 row.max=1
-    //% group="LCD Display"
-    export function lcdWrite(text: string, col: number, row: number): void {
-        let pos = 0x80 + (row * 0x40) + col
-        lcdCommand(pos)
+    // send data
+    function dat(d: number) {
+        RS = 1
+        set(d)
+        set(d << 4)
+    }
 
-        for (let i = 0; i < text.length; i++) {
-            lcdWriteChar(text.charCodeAt(i))
+    // auto get LCD address
+    function AutoAddr() {
+        let k = true
+        let addr = 0x20
+        let d1 = 0, d2 = 0
+        while (k && (addr < 0x28)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
+        }
+        if (!k) return addr
+
+        addr = 0x38
+        while (k && (addr < 0x40)) {
+            pins.i2cWriteNumber(addr, -1, NumberFormat.Int32LE)
+            d1 = pins.i2cReadNumber(addr, NumberFormat.Int8LE) % 16
+            pins.i2cWriteNumber(addr, 0, NumberFormat.Int16LE)
+            d2 = pins.i2cReadNumber(addr, NumberFormat.Int8LE)
+            if ((d1 == 7) && (d2 == 0)) k = false
+            else addr += 1
+        }
+        if (!k) return addr
+        else return 0
+
+    }
+
+    /**
+     * initial LCD, set I2C address. Address is 39/63 for PCF8574/PCF8574A
+     * @param Addr is i2c address for LCD, eg: 0, 39, 63. 0 is auto find address
+     */
+    //% blockId="I2C_LCD1620_SET_ADDRESS" block="LCD initialize with Address %addr"
+    //% weight=100 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function LcdInit(Addr: number) {
+        if (Addr == 0) i2cAddr = AutoAddr()
+        else i2cAddr = Addr
+        BK = 8
+        RS = 0
+        cmd(0x33)       // set 4bit mode
+        basic.pause(5)
+        set(0x30)
+        basic.pause(5)
+        set(0x20)
+        basic.pause(5)
+        cmd(0x28)       // set mode
+        cmd(0x0C)
+        cmd(0x06)
+        cmd(0x01)       // clear
+    }
+
+    /**
+     * show a number in LCD at given position
+     * @param n is number will be show, eg: 10, 100, 200
+     * @param x is LCD column position, eg: 0
+     * @param y is LCD row position, eg: 0
+     */
+    //% blockId="I2C_LCD1620_SHOW_NUMBER" block="show number %n|at x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    //% parts=LCD1602_I2C trackArgs=0
+    export function ShowNumber(n: number, x: number, y: number): void {
+        let s = n.toString()
+        ShowString(s, x, y)
+    }
+
+    /**
+     * show a string in LCD at given position
+     * @param s is string will be show, eg: "Hello"
+     * @param x is LCD column position, [0 - 15], eg: 0
+     * @param y is LCD row position, [0 - 1], eg: 0
+     */
+    //% blockId="I2C_LCD1620_SHOW_STRING" block="show string %s|at x %x|y %y"
+    //% weight=90 blockGap=8
+    //% x.min=0 x.max=15
+    //% y.min=0 y.max=1
+    //% parts=LCD1602_I2C trackArgs=0
+    export function ShowString(s: string, x: number, y: number): void {
+        let a: number
+
+        if (y > 0)
+            a = 0xC0
+        else
+            a = 0x80
+        a += x
+        cmd(a)
+
+        for (let i = 0; i < s.length; i++) {
+            dat(s.charCodeAt(i))
         }
     }
 
-    // Clear LCD
-    //% block="Clear LCD"
-    //% group="LCD Display"
-    export function lcdClear(): void {
-        lcdCommand(0x01)
-        basic.pause(5)
+    /**
+     * turn on LCD
+     */
+    //% blockId="I2C_LCD1620_ON" block="turn on LCD"
+    //% weight=81 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function on(): void {
+        cmd(0x0C)
     }
 
+    /**
+     * turn off LCD
+     */
+    //% blockId="I2C_LCD1620_OFF" block="turn off LCD"
+    //% weight=80 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function off(): void {
+        cmd(0x08)
+    }
+
+    /**
+     * clear all display content
+     */
+    //% blockId="I2C_LCD1620_CLEAR" block="clear LCD"
+    //% weight=85 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function clear(): void {
+        cmd(0x01)
+    }
+
+    /**
+     * turn on LCD backlight
+     */
+    //% blockId="I2C_LCD1620_BACKLIGHT_ON" block="turn on backlight"
+    //% weight=71 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function BacklightOn(): void {
+        BK = 8
+        cmd(0)
+    }
+
+    /**
+     * turn off LCD backlight
+     */
+    //% blockId="I2C_LCD1620_BACKLIGHT_OFF" block="turn off backlight"
+    //% weight=70 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function BacklightOff(): void {
+        BK = 0
+        cmd(0)
+    }
+
+    /**
+     * shift left
+     */
+    //% blockId="I2C_LCD1620_SHL" block="Shift Left"
+    //% weight=61 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function shl(): void {
+        cmd(0x18)
+    }
+
+    /**
+     * shift right
+     */
+    //% blockId="I2C_LCD1620_SHR" block="Shift Right"
+    //% weight=60 blockGap=8
+    //% parts=LCD1602_I2C trackArgs=0
+    export function shr(): void {
+        cmd(0x1C)
+    }
 }
