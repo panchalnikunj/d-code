dba91305cfc7d9831ab3ca61fb2663c53c06a5b0
Nikunj Panchal <59598242+panchalnikunj@users.noreply.github.com>
2025-03-06 19:33:03 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 /**
  * Custom blocks
  */
-//% weight=20 color=#0fbc11 icon="U+1F916"
+//% weight=20 color=#0fbc11 icon="\U1F916"
 namespace I2C_LCD1602 {
     let i2cAddr: number // 0x3F: PCF8574A, 0x27: PCF8574
     let BK: number      // backlight control
